59b9b2924ec7790d68450c04ecfa9793a3af775b
LUONGNV <luongnv@LUONG-2.local>
2020-11-28 09:27:39 +0700

    recrawl

--- index.js
+++ index.js
@@ -2,21 +2,23 @@ require("dotenv").config();
 const axios = require("axios");
 const fs = require("fs");
 const cheerio = require("cheerio");
-const csvWriter = require("csv-write-stream");
 const _ = require("lodash");
+const stringify = require("csv-stringify");
 
 const env = process.env;
 
-const writeStreamFile = (stream) => {
-  const writeStream = fs.createWriteStream(__dirname + `/designs.csv`);
-  new Promise((resolve, reject) => {
-    stream.on("data", (chunk) => writeStream.write(chunk));
-    stream.on("error", reject);
-    stream.on("end", resolve);
+const writeFile = (row) => {
+  stringify([row], (err, output) => {
+    const file = __dirname + `/designs.csv`;
+    fs.appendFile(file, output, "utf8", (err) => {
+      if (err) {
+        console.log("err: ", err);
+      }
+    });
   });
 };
 
-const tranformObj = (obj) => {
+const transform = (obj) => {
   let newObj = {};
   for (let key in obj) {
     let newKey = key
@@ -30,15 +32,7 @@ const tranformObj = (obj) => {
 };
 
 (async () => {
-  const writer = csvWriter({
-    sendHeaders: false,
-  });
-
-  writer.write({
-    id: "id",
-    data: "data",
-    published: "published",
-  });
+  writeFile(["id", "data", "published"]);
   let itemIndex = 0;
   let idx = 0;
   do {
@@ -61,7 +55,7 @@ const tranformObj = (obj) => {
     }
     const links = str.match(/id=+\d+&shop_r=1sttheworld.myshopify.com/gi);
     for (const item of links) {
-      const resl = await axios.get(
+      const response = await axios.get(
         `https://doshopify.com/product-personalizer/manage.php?${item}`,
         {
           headers: {
@@ -69,50 +63,75 @@ const tranformObj = (obj) => {
           },
         }
       );
-      const dt = resl.data.toString();
+      const dt = response.data.toString();
       const $ = cheerio.load(dt);
       const id = item.match(/\d+/g)[0];
-      let dataCrawl = {};
-      $("div.form-group").each((i, form) => {
-        const label = $(form).find("label").text();
-        if (!label) return;
-        let itemData = "";
-        const option = $(form).find(`option[selected]`).text();
-        if (option) {
-          if (option.match(/.png|.jpg|.jpeg/g)) {
-            itemData = `https://doshopify.com/product-personalizer/${$(form)
-              .find(`option`)
-              .val()}`;
+      let personalize = {};
+      const enableCustomization = $(
+        `div.checkbox label input[name="cstmfy_req"]`
+      );
+      if (enableCustomization) {
+        personalize.enable_customization =
+          enableCustomization.val() == 1 ? true : false;
+      }
+      const allVariantImage = $(`div.checkbox label input[name="cstmfy_all"]`);
+      if (allVariantImage) {
+        personalize.all_variant_image =
+          allVariantImage.val() == 1 ? true : false;
+      }
+      const tabs = $("div.tab-pane.in.clone");
+      let blocks = [];
+      tabs.each((i, tab) => {
+        let tabData = {};
+        const forms = $(tab).find("div.form-group");
+        forms.each((i, form) => {
+          const label = $(form).find("label").text();
+          if (!label) return;
+          let propData = "";
+          const option = $(form).find(`option[selected]`).text();
+          if (option) {
+            if (option.match(/.png|.jpg|.jpeg/g)) {
+              propData = `https://doshopify.com/product-personalizer/${$(form)
+                .find(`option`)
+                .val()}`;
+            } else {
+              propData = option == `--Select--` ? null : option;
+            }
           } else {
-            itemData = option;
+            const op = $(form).find(`option`).val();
+            if (op) {
+              propData = op;
+            }
           }
-        } else {
-          const op = $(form).find(`option`).val();
-          if (op) {
-            itemData = op;
+          const textarea = $(form).find("textarea").val();
+          if (textarea) {
+            propData = textarea;
           }
-        }
-        const textarea = $(form).find("textarea").val();
-        if (textarea) {
-          itemData = textarea;
-        }
-        const input = $(form).find("input").val();
-        if (input) {
-          itemData = input;
-        }
-        dataCrawl[label] = itemData;
+          const input = $(form).find("input").val();
+          if (input) {
+            propData = input;
+          }
+          tabData[label] = propData;
+        });
+        tabData = transform(tabData);
+        blocks.push(tabData);
       });
+      personalize.blocks = blocks;
+      const svpForms = $(`div.svp div.form-group`);
+      svpForms.each((i, svp) => {
+        const label = $(svp).find("label").text();
+        personalize[label] =
+          $(svp).find("option[selected]").text() || $(svp).find("input").val();
+      });
+      personalize = transform(personalize);
       console.log("CRAWLING ITEM: ", itemIndex);
       itemIndex++;
-      const tfData = tranformObj(dataCrawl);
-      writer.write({
+      writeFile([
         id,
-        data: JSON.stringify(tfData),
-        published: (tfData.enable_customization = 1 ? true : false),
-      });
+        JSON.stringify(personalize),
+        personalize.enable_customization ? "true" : "false",
+      ]);
     }
-    await writeStreamFile(writer);
     idx += 20;
   } while (idx > -1);
-  writer.end();
 })();
